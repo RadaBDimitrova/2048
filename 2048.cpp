3d7749fb0f76dc488977bf37b57c8d39faa5b99a
RadaBDimitrova <117522044+RadaBDimitrova@users.noreply.github.com>
2023-01-18 00:09:14 +0200

    Add files via upload

--- 2048.cpp
+++ 2048.cpp
@@ -32,15 +32,31 @@ int score(int** arr, int size) {
 }
 
 void updateBoard(int** arr, int size) {
+    int x1 = rand() % size;
+    int y1 = rand() % size;
+    int x2 = rand() % size;
+    int y2 = rand() % size;
 
+    //in case the coordinates of the first two tiles are the same
+    while (x2 == x1 && y2 == y1) { 
+        x2 = rand() % size;
+        y2 = rand() % size;
+    }
+    arr[x1][y1] = 2;
+    arr[x2][y2] = 2;
 }
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    char nickname[100];
+    int dimension = 0;
+    std::cout << "Enter your nickname:";
+    std::cin >> nickname;
+    std::cout << "Enter dimension";
+    std::cin >> dimension;
     while (true)
     {
-        printBoard(board, N);
+        printBoard(board, dimension);
         char command;
         std::cin >> command;
     }
