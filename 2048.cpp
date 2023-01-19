1ae84769040c4b898fdc3348753d4f4f3a9ea06c
RadaBDimitrova <117522044+RadaBDimitrova@users.noreply.github.com>
2023-01-19 19:59:09 +0200

    Add files via upload
    Directions mapped by "w" and "s" work
    Directions mapped by "a" and "d" are a work in progress

--- 2048.cpp
+++ 2048.cpp
@@ -87,162 +87,199 @@ void insertNewTile(int** arr, int size) {
 	}
 }
 
-void moveTiles(int** arr, int size) {
-	int score = 0;
-	char direction;
-	std::cin >> direction;
-	while (direction != 'w' && direction != 's' && direction != 'd' && direction != 'a')
+void mergeUpDown(int** arr, int size) {
+	for (int j = 0; j < size; j++)
 	{
-		std::cin >> direction;
-	}
-	switch (direction)
-	{
-	case 'w':
 		for (int i = 0; i < size - 1; i++)
 		{
-			for (int j = 0; j < size; j++)
+			for (int row = i + 1; row < size; row++)
 			{
-				int counter = 0;
-				while (arr[i][j] == 0 && counter < size)
+				if (arr[row][j] == 0)
 				{
-					counter++;
-					for (int row = i; row < size - 1; row++)
-					{
-						if (arr[row][j] == arr[row + 1][j]) {
-							arr[row][j] *= 2;
-							arr[row + 1][j] = 0;
-						}
-						arr[row][j] = arr[row + 1][j];
-						arr[row + 1][j] = 0;
-					}
-					arr[size - 1][j] = 0;
-					/*int row = i;
-					while (row < size - 1)
-					{
-						arr[row][j] = arr[row + 1][j];
-						arr[row + 1][j] = 0;
-						row++;
-					}
-					arr[size - 1][j] = 0;*/
+					continue;
 				}
-				if (arr[i][j] == arr[i + 1][j]) {
+				if (arr[i][j] == 0)
+				{
+					arr[i][j] = arr[row][j];
+					arr[row][j] = 0;
+				}
+				else if (arr[i][j] == arr[row][j])
+				{
 					arr[i][j] *= 2;
-					arr[i + 1][j] = 0;
-					//score += arr[i][j];
+					arr[row][j] = 0;
 				}
 			}
 		}
-		break;
-	case's':
-
-		break;
-	case 'a':
-
-		break;
-	case 'd':
-
-		break;
-	default:
-		break;
 	}
 }
 
 
-bool win(int** arr, int size) {
-	int countEmptySlots = 0;
-	for (int i = 0; i < size; i++)
+void moveUp(int** arr, int size) {
+	for (int i = 0; i < size - 1; i++)
 	{
 		for (int j = 0; j < size; j++)
 		{
-			if (arr[i][j] == 2048)
+			int counter = 0;
+			while (arr[i][j] == 0 && counter < size)
 			{
-				return true;
+				counter++;
+				for (int row = i; row < size - 1; row++)
+				{
+					arr[row][j] = arr[row + 1][j];
+					arr[row + 1][j] = 0;
+				}
+				arr[size - 1][j] = 0;
 			}
 		}
 	}
-	return false;
 }
 
 
-void drawBoard(int** arr, int size) {
-	if (emptyArray(arr, size))
+void moveDown(int** arr, int size) {
+	for (int i = size - 1; i > 0; i--)
 	{
+		for (int j = 0; j < size; j++)
+		{
+			int counter = 0;
+			while (arr[i][j] == 0 && counter < size)
+			{
+				counter++;
+				for (int row = i; row > 0; row--)
+				{
+					arr[row][j] = arr[row - 1][j];
+					arr[row - 1][j] = 0;
+				}
+				arr[0][j] = 0;
+			}
+		}
+	}
+}
+
+	void moveTiles(int** arr, int size) {
+		int score = 0;
+		char direction;
+		std::cin >> direction;
+		while (direction != 'w' && direction != 's' && direction != 'd' && direction != 'a')
+		{
+			std::cin >> direction;
+		}
+		switch (direction)
+		{
+		case 'w':
+			mergeUpDown(arr, size);
+			moveUp(arr, size);
+			break;
+		case's':
+			mergeUpDown(arr, size);
+			moveDown(arr, size);
+			break;
+		case 'a':
+
+			break;
+		case 'd':
+
+			break;
+		default:
+			break;
+		}
+	}
+
+
+	bool win(int** arr, int size) {
+		int countEmptySlots = 0;
+		for (int i = 0; i < size; i++)
+		{
+			for (int j = 0; j < size; j++)
+			{
+				if (arr[i][j] == 2048)
+				{
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+
+	void drawBoard(int** arr, int size) {
+		if (emptyArray(arr, size))
+		{
+			insertNewTile(arr, size);
+		}
 		insertNewTile(arr, size);
-	}
-	insertNewTile(arr, size);
-	printBoard(arr, size);
-	moveTiles(arr, size);
+		printBoard(arr, size);
+		moveTiles(arr, size);
 
-	if (win(arr, size))
-	{
-		std::cout << "Congrats! You got 2048!" << std::endl;
+		if (win(arr, size))
+		{
+			std::cout << "Congrats! You got 2048!" << std::endl;
+			return;
+		}
+		else if (!checkPossibleMoves(arr, size))
+		{
+			std::cout << "Game over!" << std::endl;
+			return;
+		}
+		else
+		{
+			clearConsole();
+			std::cout << "Score: " << score(arr, size) << std::endl; // prints the previous board sum
+			drawBoard(arr, size);
+		}
+	}
+
+	void startGame() {
+		char* nickname = new char[100];
+		int dimension = 0;
+		std::cout << "Enter your nickname: ";
+		std::cin >> nickname;
+		std::cout << "Enter dimension: ";
+		std::cin >> dimension;
+
+		int** board = new int* [dimension];
+		for (int i = 0; i < dimension; i++)
+		{
+			board[i] = new int[dimension]();
+		}
+
+		drawBoard(board, dimension);
+
+		for (int i = 0; i < dimension; i++) {
+			delete[] board[i];
+		}
+		delete[] board;
+		delete[] nickname;
 		return;
 	}
-	else if (!checkPossibleMoves(arr, size))
-	{
-		std::cout << "Game over!" << std::endl;
-		return;
-	}
-	else
-	{
-	clearConsole();
-	std::cout << "Score: " << score(arr, size) << std::endl; // prints the previous board sum
-	drawBoard(arr, size);
-	}
-}
 
-void startGame() {
-	char* nickname = new char[100];
-	int dimension = 0;
-	std::cout << "Enter your nickname: ";
-	std::cin >> nickname;
-	std::cout << "Enter dimension: ";
-	std::cin >> dimension;
-
-	int** board = new int* [dimension];
-	for (int i = 0; i < dimension; i++)
-	{
-		board[i] = new int[dimension]();
-	}
-
-	drawBoard(board, dimension);
-
-	for (int i = 0; i < dimension; i++) {
-		delete[] board[i];
-	}
-	delete[] board;
-	delete[] nickname;
-	return;
-}
-
-void startMenu() {
-	std::cout << "1. Start game" << std::endl;
-	std::cout << "2. Leaderboard" << std::endl;
-	std::cout << "3. Quit" << std::endl;
-	int choice;
-	std::cin >> choice;
-	while (choice != 1 && choice != 2 && choice != 3)
-	{
-		std::cout << "Invalid choice. Please enter a number corresponding to the menu options." << std::endl;
+	void startMenu() {
+		std::cout << "1. Start game" << std::endl;
+		std::cout << "2. Leaderboard" << std::endl;
+		std::cout << "3. Quit" << std::endl;
+		int choice;
 		std::cin >> choice;
+		while (choice != 1 && choice != 2 && choice != 3)
+		{
+			std::cout << "Invalid choice. Please enter a number corresponding to the menu options." << std::endl;
+			std::cin >> choice;
+			clearConsole();
+		}
 		clearConsole();
+		switch (choice)
+		{
+		case 1:
+			startGame();
+			break;
+		case 2: //open menu for leaderboard options
+			break;
+		case 3:
+			return;
+		default:
+			break;
+		}
 	}
-	clearConsole();
-	switch (choice)
-	{
-	case 1:
-		startGame();
-		break;
-	case 2: //open menu for leaderboard options
-		break;
-	case 3:
-		return;
-	default:
-		break;
-	}
-}
 
-int main()
-{
-	startMenu();
-}
+	int main()
+	{
+		startMenu();
+	}
